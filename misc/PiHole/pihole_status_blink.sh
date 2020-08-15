9fd660e6d8a5fa06627f59c897e04b3e9c0490a7
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2020-08-15 09:57:05 -0700

    use servertickle for pihole status blink

--- misc/PiHole/pihole_status_blink.sh
+++ misc/PiHole/pihole_status_blink.sh
@@ -3,19 +3,20 @@
 # install blink1-tool: https://github.com/todbot/blink1-tool/releases
 # install bc : sudo apt-get install bc
 
+blink1-tool -q --clearpattern
+blink1-tool -q -m 1000 --red --setpattline 0
+blink1-tool -q -m 1000 --off --setpattline 1
+blink1-tool -q --savepattern
+
+
 while [ 1 ]; do
     loadavg=`uptime | awk '{print $10+0}'`
 
     if pihole status | grep ✗ > /dev/null; then
-        blink1-tool -q --red -m 1000
-    elif (( $(echo "$loadavg > 1" |bc -l) )); then
-        /blink1-tool -q --red -m 1000
+        blink1-tool -q --magenta -m 1000
     elif (( $(echo "$loadavg > 0.8" |bc -l) )); then
         blink1-tool -q --yellow -m 1000
-    else
-        blink1-tool -q --green -m 1000
     fi
-    sleep 2
-    blink1-tool -q --off -m 1000
+    blink1-tool -t 3000 --servertickle 1
     sleep 2
 done

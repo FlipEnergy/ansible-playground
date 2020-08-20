2215a5f0196ae022bc568e1d5c720c52518c6968
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2020-08-20 15:41:30 -0700

    better status blink

--- misc/PiHole/pihole_status_blink.sh
+++ misc/PiHole/pihole_status_blink.sh
@@ -3,20 +3,32 @@
 # install blink1-tool: https://github.com/todbot/blink1-tool/releases
 # install bc : sudo apt-get install bc
 
+blink1-tool --servertickle 0
+
 blink1-tool -q --clearpattern
-blink1-tool -q -m 1000 --red --setpattline 0
-blink1-tool -q -m 1000 --off --setpattline 1
+blink1-tool -q -m 2000 --red --setpattline 0
+blink1-tool -q -m 2000 --off --setpattline 1
 blink1-tool -q --savepattern
 
+# Startup light
+for h in {0..15}; do
+    ((hue=h*16))
+    blink1-tool -l 2 --hsb $hue,255,255
+    blink1-tool -l 1 --hsb $hue_old,255,255
+    hue_old=$hue
+    sleep 0.1
+done
 
 while [ 1 ]; do
     loadavg=`uptime | awk '{print $10+0}'`
 
     if pihole status | grep ✗ > /dev/null; then
         blink1-tool -q --magenta -m 1000
+        blink1-tool -q --off -m 1000
     elif (( $(echo "$loadavg > 0.8" |bc -l) )); then
         blink1-tool -q --yellow -m 1000
+        blink1-tool -q --off -m 1000
     fi
-    blink1-tool -t 3000 --servertickle 1
     sleep 2
+    blink1-tool -t 5000 --servertickle 1
 done

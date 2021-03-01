994247bd898c966c8de28d1e36f94155839caa70
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-03-01 10:34:24 -0800

    add music to back ups

--- misc/backup.sh
+++ misc/backup.sh
@@ -5,7 +5,10 @@
 SOURCE_DIR='/mnt/d/Syncthing Data'
 DEST_DIR='/mnt/e/Syncthing Data'
 
+echo 'Music...'
+echo '========='
+rsync -rP '/mnt/c/Users/denni/Music/' '/mnt/e/Music'
 
-echo 'Syncing...'
+echo 'Syncthing Data...'
 echo '========='
 rsync -rP "${SOURCE_DIR%/}/" "${DEST_DIR}"

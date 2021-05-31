27c7086432ca2e410a26d2e14fb0aad924f51f6b
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-05-30 18:17:34 -0700

    augment backup script

--- misc/backup.sh
+++ misc/backup.sh
@@ -2,14 +2,36 @@
 
 # Run in WSL2
 
-DEST_DIR='/mnt/e'
+mount_offsite() {
+  mkdir -p /mnt/f
+  mount -t drvfs F: /mnt/f
+}
 
-echo "Backing up to [$DEST_DIR]"
+backup() {
+  DEST_DIR=$1
 
-echo 'Music...'
-echo '========='
-rsync -avh --delete '/mnt/c/Users/denni/Music/' "${DEST_DIR%/}/Music"
+  echo "Backing up to [$DEST_DIR]"
 
-echo 'Syncthing Data...'
-echo '========='
-rsync -avh --delete '/mnt/d/Syncthing Data' "${DEST_DIR%/}/Syncthing Data"
+  echo 'Music...'
+  echo '========='
+  rsync -avh --delete '/mnt/c/Users/denni/Music/' "${DEST_DIR%/}/Music"
+
+  echo 'Syncthing Data...'
+  echo '========='
+  rsync -avh --delete '/mnt/d/Syncthing Data' "${DEST_DIR%/}/Syncthing Data"
+}
+
+if [ "$EUID" -ne 0 ]
+  then echo "Run as root!"
+  exit 1
+fi
+
+case $1 in
+  -c|--cold)
+    mount_offsite
+    backup /mnt/f
+    ;;
+  -w|--warm|*)
+    backup /mnt/e
+    ;;
+esac

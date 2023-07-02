3bb7a1208e7d1bbb55a89dfb34773db9983b4ffc
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2023-07-02 23:00:55 +0000

    3rd lcd

--- roles/site_node/files/k8s_stats_lcd/k8s_stats_lcd.py
+++ roles/site_node/files/k8s_stats_lcd/k8s_stats_lcd.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(
         prog='k8s_stats_lcd',
         description='Display k8s stats on an LCD display')
-    parser.add_argument('stats', choices=['pods', 'nodes'], help='the stats to display')
+    parser.add_argument('stats', choices=['pods', 'nodes', 'resources'], help='the stats to display')
     args = parser.parse_args()
 
     lcd = CharLCD('PCF8574', scan_i2c_bus())
@@ -47,6 +47,20 @@ if __name__ == "__main__":
                 output = os.popen(f'k3s kubectl get --no-headers -A --field-selector=status.phase={status} pods').read()
                 data.append(str(output.count('\n')))
         elif args.stats == 'nodes':
+            ljust_size = 6
+            data = [0, 0]
+            headers = ['Ready', 'Not']
+            output = os.popen(f'k3s kubectl get --no-headers nodes').read().split('\n')
+            for node in output:
+                node_list = node.split()
+                if len(node_list) < 3: # kubectl didn't succeed
+                    break
+                elif node_list[1] == 'Ready':
+                    data[0] += 1
+                else:
+                    data[1] += 1
+            data = [str(data[0]), str(data[1])]
+        elif args.stats == 'resources':
             ljust_size = 6
             headers = ['CPU', 'RAM']
             output = os.popen(f'k3s kubectl top node --no-headers').read()
